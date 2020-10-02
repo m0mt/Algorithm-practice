595f173dcc198936e318f40dc31d6c97cbe90246
m0mt <ycross4008@gmail.com>
2020-10-02 21:44:35 +0900

    Add 42587.py

--- python/programmers/lv2/42587.py
+++ python/programmers/lv2/42587.py
@@ -1,8 +1,20 @@
 def solution(priorities, location):
-    answer = 0
-
-    document = [0 for _ in priorities]
+    index_location = [i for i in range(len(priorities))]
+    final_location = []
     
+    while len(priorities):
+        max_prirority = max(priorities)
+        current_priority = priorities.pop(0)
+
+        if current_priority == max_prirority:
+            final_location.append(index_location.pop(0))
+
+        else :
+            priorities.append(current_priority)
+            index_location.append(index_location.pop(0))
+    
+    answer = final_location.index(location) + 1
+    print(final_location)
     return answer
 
-print(solution([2,1,3,2], 2))
+print(solution(	[1, 1, 9, 1, 1, 1], 0))
