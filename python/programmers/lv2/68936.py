7d19a9ab4f7578037ffc8f6affa8ba8e5c7fe910
m0mt <ycross4008@gmail.com>
2020-11-11 22:29:11 +0900

    Add 68936.py First solution'

---
+++ python/programmers/lv2/68936.py
@@ -0,0 +1,22 @@
+def solution(arr):
+    answer = [0,0]
+    l = len(arr)
+    def quad(x, y, n):
+        init = arr[x][y]
+        for i in range(x, x + n):
+            for j in range(y, y + n):
+                if arr[i][j] != init :
+                    n_ = n // 2 
+                    quad(x, y, n_)
+                    quad(x + n_, y, n_)
+                    quad(x, y + n_, n_)
+                    quad(x + n_, y + n_, n_)
+                    return
+        answer[init] += 1
+        return
+        
+    quad(0,0,l)
+    return answer
+
+print(solution([[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]]))
+print(solution([[1,1,1,1,1,1,1,1],[0,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1],[0,1,0,0,1,1,1,1],[0,0,0,0,0,0,1,1],[0,0,0,0,0,0,0,1],[0,0,0,0,1,0,0,1],[0,0,0,0,1,1,1,1]]))
